fc0c03c025ca116efedea50bc7da59dc819df634
Adnan Ashraf <adnandev604@gmail.com>
2025-08-12 11:28:03 -0700

    updated-be

--- lib/controllers/add_todo_contoller.dart
+++ lib/controllers/add_todo_contoller.dart
@@ -2,24 +2,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-
-
-class AddTodoController{
+class AddTodoController {
   final titleController = TextEditingController();
   final descriptionController = TextEditingController();
   final _auth = FirebaseAuth.instance;
   final _firestore = FirebaseFirestore.instance;
 
-
-  
-  Future<void> savetodo(BuildContext context)async{
-    User? user = _auth.currentUser;
-  await _firestore.collection('users').doc(user!.uid).collection('todos').add({
-    'title':titleController.text,
-    'description':descriptionController.text,
-    'createdAt':FieldValue.serverTimestamp(),
-  });
-  Navigator.pop(context);
-}
-  
+  Future<void> savetodo(BuildContext context) async {
+    try {
+      User? user = _auth.currentUser;
+      await _firestore
+          .collection('users')
+          .doc(user!.uid)
+          .collection('todos')
+          .add({
+            'title': titleController.text,
+            'description': descriptionController.text,
+            'createdAt': FieldValue.serverTimestamp(),
+          });
+      Navigator.pop(context);
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Error saving todo: $e')));
+    }
+  }
 }
